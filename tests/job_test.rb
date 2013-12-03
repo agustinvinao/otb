a9f6cce6831be11585f01df80facf040f871087c
Agustin Viñao <agustinvinao@gmail.com>
2013-12-03 12:12:30 -0300

    Initial commit

---
+++ tests/job_test.rb
@@ -0,0 +1,7 @@
+require_relative 'minitest_helper'
+require 'job'
+class JobTest < Minitest::Test
+  def test_true
+    assert_equal true, true
+  end
+end
---
+++ tests/queue_test.rb
@@ -0,0 +1,7 @@
+require_relative 'minitest_helper'
+require 'queue'
+class QueueTest < Minitest::Test
+  def test_true
+    assert_equal true, true
+  end
+end
